0526f8543640b9499d0cffcb9d2b4cc47c90873a
André <AndreR@ppedv.de>
2023-06-14 11:56:42 +0200

    Template mit WinForms

---
+++ WinFormsTemplate/WinFormsTemplate/MyButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTemplate
+{
+    internal class MyButton : Button
+    {
+        int c = 0;
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.FillRectangle(new SolidBrush(Parent.BackColor), this.ClientRectangle);
+            pevent.Graphics.FillEllipse(Brushes.YellowGreen, this.ClientRectangle);
+
+            var sf = new StringFormat() { Alignment = StringAlignment.Center,LineAlignment=StringAlignment.Center};
+            pevent.Graphics.DrawString(Text+$"{c++}", SystemFonts.DefaultFont, new SolidBrush(ForeColor), ClientRectangle,sf);
+        }
+    }
+}
