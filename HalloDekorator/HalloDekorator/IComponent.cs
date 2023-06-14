bc3ebf7e7fd43016142f413bcd3d4e2faadd65c3
André <AndreR@ppedv.de>
2023-06-14 11:22:44 +0200

    Deko macht hübsch

---
+++ HalloDekorator/HalloDekorator/IComponent.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata;
+
+namespace HalloDekorator
+{
+    internal interface IComponent
+    {
+        decimal Preis { get; }
+        string Name { get; }
+    }
+
+    class Pizza : IComponent
+    {
+        public decimal Preis => 6m;
+
+        public string Name => "Pizza";
+    }
+
+    class Brot : IComponent
+    {
+        public decimal Preis => 3m;
+
+        public string Name => "Brot";
+    }
+
+    abstract class Dekorator : IComponent
+    {
+        protected readonly IComponent component;
+
+        public Dekorator(IComponent component)
+        {
+            this.component = component;
+        }
+
+        public abstract decimal Preis { get; }
+
+        public abstract string Name { get; }
+    }
+
+    class Salami : Dekorator
+    {
+        public Salami(IComponent component) : base(component)
+        { }
+
+        public override decimal Preis => component.Preis + 3m;
+
+        public override string Name => component.Name + " Salami";
+    }
+
+    class Käse : Dekorator
+    {
+        public Käse(IComponent component) : base(component)
+        { }
+
+        public override decimal Preis => component.Preis + 2m;
+
+        public override string Name => component.Name + " Käse";
+    }
+}
---
+++ HalloDekorator/HalloDekorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using HalloDekorator;
+
+Console.WriteLine("Hello, World!");
+
+
+var myPizza = new Salami(new Pizza());
+
+Console.WriteLine($"{myPizza.Name} {myPizza.Preis}");
+
+var myBrot = new Käse(new Käse(new Salami(new Brot())));
+
+Console.WriteLine($"{myBrot.Name} {myBrot.Preis}");
