d279d86739308cbd25f8acd4dd033c192d8b293d
André <AndreR@ppedv.de>
2023-06-14 09:53:32 +0200

    Singelton ist fein

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace HalloSingelton
+{
+    public class Logger
+    {
+        static Logger instance;
+        private static object instanceLock = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                    lock (instanceLock)
+                    {
+                        if (instance == null)
+                            instance = new Logger();
+                    }
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            Info("new Logger");
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine($"{DateTime.Now:G} [INFO] {message}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,10 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 10; i++)
+{
+    Task.Run(() => Logger.Instance.Info($"Hallo {i}"));
+}
+
+Logger.Instance.Info("Was geht?");
