902f2ea3c95746458250e3a0191168d996b67b6b
André <AndreR@ppedv.de>
2023-06-14 16:24:40 +0200

    EfCore mit lecker Tests

---
+++ ppedv.KurvenkönigsKarren/ppedv.KurvenkönigsKarren.Data.Database.Tests/EfContextTests.cs
@@ -0,0 +1,64 @@
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
+using ppedv.KurvenkönigsKarren.Model;
+using System.Reflection;
+
+namespace ppedv.KurvenkönigsKarren.Data.Database.Tests
+{
+    public class EfContextTests
+    {
+        readonly string conString = "Server=(localdb)\\mssqllocaldb;Database=KurvenKönig_Test;Trusted_Connection=true;";
+
+        [Fact]
+        public void Can_create_DB()
+        {
+            var con = new EfContext(conString);
+            con.Database.EnsureDeleted();
+
+            Assert.True(con.Database.EnsureCreated());
+        }
+
+        [Fact]
+        public void Insert_and_read_Car()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new PropertyNameOmitter(nameof(Entity.Id)));
+            var car = fix.Create<Car>();
+
+            using (var con = new EfContext(conString))
+            {
+                con.Database.EnsureCreated();
+                con.Add(car);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext(conString))
+            {
+                var loaded = con.Find<Car>(car.Id);
+                loaded.Should().BeEquivalentTo(car, x => x.IgnoringCyclicReferences());
+            }
+
+        }
+    }
+
+    internal class PropertyNameOmitter : ISpecimenBuilder
+    {
+        private readonly IEnumerable<string> names;
+
+        internal PropertyNameOmitter(params string[] names)
+        {
+            this.names = names;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var propInfo = request as PropertyInfo;
+            if (propInfo != null && names.Contains(propInfo.Name))
+                return new OmitSpecimen();
+
+            return new NoSpecimen();
+        }
+    }
+}
---
+++ ppedv.KurvenkönigsKarren/ppedv.KurvenkönigsKarren.Data.Database/EfContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.KurvenkönigsKarren.Model;
+
+namespace ppedv.KurvenkönigsKarren.Data.Database
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+
+        readonly string conString;
+
+        public EfContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().Property(x => x.Color).HasColumnName("Farbe").HasMaxLength(95);
+
+            modelBuilder.Entity<Rent>().HasOne(x => x.Driver).WithMany(x => x.RentsAsDriver);
+            modelBuilder.Entity<Customer>().HasMany(x => x.RentsAsPayer).WithOne(x => x.Payer);
+        }
+
+    }
+}
