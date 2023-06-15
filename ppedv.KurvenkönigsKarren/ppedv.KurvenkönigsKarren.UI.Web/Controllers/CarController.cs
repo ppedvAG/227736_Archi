37d9d1d7e950ab4931768cd4288e872ebada77dc
André <AndreR@ppedv.de>
2023-06-15 16:45:40 +0200

    ASP MVC

---
+++ ppedv.KurvenkönigsKarren/ppedv.KurvenkönigsKarren.UI.Web/Controllers/CarController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.KurvenkönigsKarren.Model.Contracts;
+using ppedv.KurvenkönigsKarren.Model.DomainModel;
+
+namespace ppedv.KurvenkönigsKarren.UI.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IRepository repo;
+
+        public CarController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: CarController
+        public ActionResult Index()
+        {
+            var cars = repo.GetAll<Car>();
+
+            return View(cars);
+        }
+
+        // GET: CarController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CarController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CarController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
