cf54d41bfd6534eb95a9bcc163fa92a283b9670c
André <AndreR@ppedv.de>
2023-06-16 10:29:22 +0200

    REST API Client

---
+++ GoogleBooksClient/GoogleBooksClient/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GoogleBooksClient;
+
+Console.WriteLine("Hello, World!");
+
+var url = "https://www.googleapis.com/books/v1/volumes?q=softwaretests";
+
+var httpClient = new HttpClient();
+
+var json = httpClient.GetStringAsync(url).Result;
+
+//Console.WriteLine(json);
+
+
+BooksResult result = System.Text.Json.JsonSerializer.Deserialize<BooksResult>(json);
+
+foreach (var item in result.items)
+{
+    Console.WriteLine($"{item.volumeInfo.title}");
+}
